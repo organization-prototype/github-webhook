3d47218ad5407d31dfde60550810c60526079281
Jimmy Huynh <jkchuynh829@gmail.com>
2017-04-14 19:24:25 -0700

    add team member generates branch

--- express_server.js
+++ express_server.js
@@ -6,6 +6,8 @@ const axios = require('axios');
 
 const github = require('express-github-webhook');
 
+const TOKEN = 'ffc5b82342d50bae557d0d8bfd62e3075747a215';
+
 const webhook = github({
   path: '/payload',
   secret: '12345'
@@ -15,8 +17,18 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(webhook);
 
-webhook.on('membership', (event, repo, data) => {
+webhook.on('membership', (repo, data) => {
+  console.log('--------EVENT---------');
 
+  console.log('member added:', data.member.login);
+
+  const input = {
+    ref: `refs/heads/${data.member.login}`,
+    sha: '777aca1faa0c57107a952303fb02b66f11eaa4a5'
+  }
+
+  axios.post(`https://api.github.com/repos/organization-prototype/github-webhook/git/refs?access_token=${TOKEN}`, input)
+  .then(data => console.log(data.data));
 });
 
 app.listen(3000, () => {
